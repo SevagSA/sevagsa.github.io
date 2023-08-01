6d08c154c38448775881696262e98cbfff28b9a9
SevagSA <sevagared2002@gmail.com>
2023-08-01 06:32:59 -0400

    removed debugging console logs

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const navBar = document.getElementById("nav-bar");
 
 hamburgerMenu.addEventListener("click", () => {
     if (navBar.getAttribute("data-nav-menu-open") == "") {
-        console.log("hos")
         navBar.removeAttribute("data-nav-menu-open")
     } else if (!navBar.getAttribute("data-nav-menu-open")) {
-        console.log("hon")
         navBar.setAttribute("data-nav-menu-open", "")
     }
 });

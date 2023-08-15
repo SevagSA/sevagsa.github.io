803ed0e8cd6488ff780d9964d93b6013485fac87
SevagSA <sevagared2002@gmail.com>
2023-08-15 07:40:49 -0400

    fixed tab panes and added Intact

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ hamburgerMenu.addEventListener("click", () => {
         navBar.setAttribute("data-nav-menu-open", "")
     }
 });
+
+
+const allTabLinks = Array.from(document.getElementsByClassName("tab-link"))
+const activeTabLinkClassName = "tab-link w-inline-block w-tab-link w--current"
+const inactiveTabLinkClassName = "tab-link w-inline-block w-tab-link"
+
+const allTabPanes = Array.from(document.getElementsByClassName("tab-pane"))
+const activePaneClassName = "tab-pane w-tab-pane w--tab-active"
+const inactivePaneClassName = "tab-pane w-tab-pane"
+
+allTabLinks.forEach(currentTabLink => {
+    currentTabLink.addEventListener("click", () => {
+        const dataWTabAttribute = currentTabLink.getAttribute("data-w-tab")
+        const currentTabPane = document.querySelectorAll(`[data-w-tab="${dataWTabAttribute}"]`)[1];
+        
+        allTabPanes.forEach((pane) => {
+            pane.setAttribute("class", inactivePaneClassName)
+        })
+
+        allTabLinks.forEach((pane) => {
+            pane.setAttribute("class", inactiveTabLinkClassName)
+        })
+
+        currentTabLink.setAttribute("class", activeTabLinkClassName)
+        currentTabPane.removeAttribute("style")
+        currentTabPane.setAttribute("class", activePaneClassName)
+    });
+});
